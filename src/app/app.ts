b95ac674d40843ab715fc3437610198afd621cf8
Srini Kusunam <skusunam@moneygram.com>
2015-10-25 19:06:27 -0500

    Initial checkin

---
+++ src/app/app.ts
@@ -0,0 +1,14 @@
+import {Component, bootstrap} from 'angular2/angular2';
+
+
+@Component({
+	selector: 'my-app',
+	template: `
+		<h1>Hello Angular2 ....</h1>
+	`
+})
+class AppComponent {
+	
+}
+
+bootstrap(AppComponent);
